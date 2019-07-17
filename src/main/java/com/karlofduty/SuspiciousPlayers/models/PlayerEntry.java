50ff0339e4a742ab8a1011aa455836457258a59e
Karl Essinger <xkaess22@gmail.com>
2019-07-17 07:38:50 +0200

    Added player history cache

--- src/main/java/com/karlofduty/SuspiciousPlayers/models/PlayerEntry.java
+++ src/main/java/com/karlofduty/SuspiciousPlayers/models/PlayerEntry.java
@@ -8,25 +8,45 @@ import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.concurrent.TimeUnit;
 
 import static net.md_5.bungee.api.ChatColor.*;
 
 public abstract class PlayerEntry
 {
-	public static SimpleDateFormat displayDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+	/**
+	 * Date formatting for plugin messages.
+	 */
+	public static final SimpleDateFormat displayDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+
+	/**
+	 * A cache of name history lookups.
+	 */
+	private static ConcurrentSkipListMap<UUID, ConcurrentSkipListMap<String, String>> nameCache = new ConcurrentSkipListMap<>();
+
+	private static long lastCacheClearing = System.currentTimeMillis();
+
+	/**
+	 * Gets an interactive message for a list entry with different buttons and actions depending on the type of entry.
+	 * @return A TextComponent containing the information and functionality of this entry.
+	 */
 	public abstract TextComponent getInteractiveMessage();
 
-	static String getUsername(UUID uuid)
+	/**
+	 * Gets a player's username from local server files.
+	 * @param uuid ID of the player.
+	 * @return The name of the player.
+	 */
+	public static String getUsername(UUID uuid)
 	{
 		try
 		{
@@ -38,60 +58,90 @@ public abstract class PlayerEntry
 		}
 	}
 
+	/**
+	 * Gets a TextComponent containing the name and a popup with name history of a player.
+	 * @param uuid ID of the player.
+	 * @param color Color of the player name in the text component.
+	 * @return The finished TextComponent.
+	 */
 	public static TextComponent getNameComponent(UUID uuid, ChatColor color)
 	{
 		try
 		{
-			TreeMap<String, String> usernameHistory = getUsernameHistory(uuid);
+			// Get the user from the cache
+			ConcurrentSkipListMap<String, String> usernameHistory = nameCache.get(uuid);
 
-			if(usernameHistory == null || usernameHistory.isEmpty())
+			// If the cache does not have this player in it, check the mojang api
+			if(usernameHistory == null)
 			{
-				return new TextComponent(
-						new ComponentBuilder(getUsername(uuid))
-								.color(color)
-								.event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, TextComponent.fromLegacyText(DARK_GRAY + "Known aliases:\n\n" + RED + "ERROR: COULD NOT CONNECT TO MOJANG API")))
-								.create());
+				usernameHistory = getUsernameHistory(uuid);
+
+				// If the mojang api does not return a proper result, get the username from the local server files
+				if(usernameHistory == null || usernameHistory.isEmpty())
+				{
+					// Return TextComponent with error popup
+					return new TextComponent(
+							new ComponentBuilder(getUsername(uuid))
+									.color(color)
+									.event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, TextComponent.fromLegacyText(DARK_GRAY + "Known aliases:\n" + RED + "ERROR: COULD NOT CONNECT TO MOJANG API")))
+									.create());
+				}
+
+				// Save the entry to the cache
+				cacheHistory(uuid, usernameHistory);
 			}
 
+			// Builds the hover popup section of the message
 			StringBuilder popup = new StringBuilder();
-
 			for(Map.Entry<String, String> entry : usernameHistory.entrySet())
 			{
 				popup.append('\n');
 				popup.append(GRAY);
-				popup.append(String.format("%-32s", entry.getValue()));
+				popup.append(String.format("    %-32s", entry.getValue()));
 				popup.append(DARK_GRAY);
 				popup.append(ITALIC);
-				popup.append(" ");
 				popup.append(String.format("%-20s", entry.getKey()));
 				popup.append(RESET);
 			}
 
+			// Builds the final TextComponent and returns it
 			return new TextComponent(
 					new ComponentBuilder(usernameHistory.firstEntry().getValue())
 							.color(color)
-							.event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, TextComponent.fromLegacyText(DARK_GRAY + "Known aliases:\n" + popup.toString())))
+							.event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, TextComponent.fromLegacyText(DARK_GRAY + "Known aliases:" + popup.toString())))
 							.create());
 		}
 		catch (Exception e)
 		{
+			// Print error and return TextComponent with error popup
 			e.printStackTrace();
-			return new TextComponent(TextComponent.fromLegacyText(color + getUsername(uuid)));
+			return new TextComponent(
+					new ComponentBuilder(getUsername(uuid))
+							.color(color)
+							.event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, TextComponent.fromLegacyText(DARK_GRAY + "Known aliases:\n" + RED + "ERROR: COULD NOT CONNECT TO MOJANG API")))
+							.create());
 		}
 	}
 
-	private static TreeMap<String, String> getUsernameHistory(UUID uuid)
+	/**
+	 * Contacts the Mojang API and requests the name history of a player.
+	 * @param uuid ID of the player to check.
+	 * @return A map of timestamps and player names.
+	 */
+	private static ConcurrentSkipListMap<String, String> getUsernameHistory(UUID uuid)
 	{
 		Type type = new TypeToken<HashMap<String, String>>(){}.getType();
 		Gson gson = new GsonBuilder().registerTypeAdapter(type, new NameMapDeserializer()).create();
 		String compactUUID = uuid.toString().replace("-", "");
 		try
 		{
+			// Contacts the Mojang API and requests the player's name history
 			URL url = new URL("https://api.mojang.com/user/profiles/" + compactUUID + "/names");
 			HttpURLConnection conn = (HttpURLConnection)url.openConnection();
 			BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 
-			TreeMap<String, String> nameHistory = gson.fromJson(reader, type);
+			// Deserializes the json response using the NameMapDeserializer implemented below
+			ConcurrentSkipListMap<String, String> nameHistory = gson.fromJson(reader, type);
 			reader.close();
 			conn.disconnect();
 
@@ -100,32 +150,59 @@ public abstract class PlayerEntry
 		catch (Exception e)
 		{
 			SuspiciousPlayers.instance.getLogger().severe("Error occurred while contacting MojangAPI: \n" + e);
+			return null;
 		}
-		return null;
+	}
+
+	/**
+	 * Caches a player's name history and also clears the cache if it has gotten old enough.
+	 * @param uuid ID of the player.
+	 * @param playerHistory The name history of the player, as time/name pairs.
+	 */
+	private static void cacheHistory(UUID uuid, ConcurrentSkipListMap<String, String> playerHistory)
+	{
+		if(lastCacheClearing + TimeUnit.HOURS.toMillis(6) < System.currentTimeMillis())
+		{
+			nameCache.clear();
+			lastCacheClearing = System.currentTimeMillis();
+		}
+		nameCache.put(uuid, playerHistory);
 	}
 }
 
 /**
- * The input map is actually an array of unnamed objects with a name and timestamp, this gets turned into a Map<Long, String>
+ * Deserializer for the name history package from the Mojang API.
+ * The input map is actually an array of unnamed objects with a name and (usually) timestamp, this gets turned into a Map<Long, String> below.
  */
-class NameMapDeserializer implements JsonDeserializer<TreeMap<String, String>>
+class NameMapDeserializer implements JsonDeserializer<ConcurrentSkipListMap<String, String>>
 {
 	@Override
-	public TreeMap<String, String> deserialize(JsonElement json, Type type, JsonDeserializationContext context)
+	public ConcurrentSkipListMap<String, String> deserialize(JsonElement json, Type type, JsonDeserializationContext context)
 	{
-		TreeMap<String, String> map = new TreeMap<>(Collections.reverseOrder());
-
-		for (JsonElement jItem : json.getAsJsonArray())
+		try
 		{
-			JsonObject jObject = jItem.getAsJsonObject();
-			String name = jObject.get("name").getAsString();
-			String timestamp = "-";
-			if(jObject.get("changedToAt") != null)
+			ConcurrentSkipListMap<String, String> map = new ConcurrentSkipListMap<>(Collections.reverseOrder());
+			for (JsonElement jItem : json.getAsJsonArray())
 			{
-				timestamp = PlayerEntry.displayDateFormat.format(jObject.get("changedToAt").getAsLong());
+				JsonObject jObject = jItem.getAsJsonObject();
+
+				// Gets the player name, this is always included in each object
+				String name = jObject.get("name").getAsString();
+
+				// Timestamps are not included on the player's first name so it is set to "-" in that case
+				String timestamp = "-";
+				if(jObject.get("changedToAt") != null)
+				{
+					timestamp = PlayerEntry.displayDateFormat.format(jObject.get("changedToAt").getAsLong());
+				}
+				map.put(timestamp, name);
 			}
-			map.put(timestamp, name);
+			return map;
 		}
-		return map;
+		catch (Exception e)
+		{
+			SuspiciousPlayers.instance.getLogger().severe("Error occurred while parsing json package: \n" + e);
+			return null;
+		}
 	}
 }
